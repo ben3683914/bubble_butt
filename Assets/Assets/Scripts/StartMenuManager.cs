29c839cf6b882076161071d3335f3cf3f0f47ed2
Darius <dfunches@silverstarbrands.com>
2025-01-26 10:30:51 -0600

    Added main menu

---
+++ Assets/Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Assets.Scripts
+{
+    public class StartMenuManager : MonoBehaviour
+    {
+        public GameObject Panel;
+
+        public void Play()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void OpenCredits()
+        {
+            Panel.SetActive(true);
+        }
+
+        public void CloseCredits()
+        {
+            Panel.SetActive(false);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
