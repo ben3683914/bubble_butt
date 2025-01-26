a659714e050c38ebea8f33b6222dc1add6192101
Ben Mueller <bmueller368@outlook.com>
2025-01-26 12:57:47 -0600

    menu hover

---
+++ Assets/Assets/Scripts/MenuHover.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHover : MonoBehaviour
+{
+    public Image playDefault;
+    public Image playHover;
+
+    public Image creditsDefault;
+    public Image creditsHover;
+
+    public Image quitDefault;
+    public Image quitHover;
+
+
+}
