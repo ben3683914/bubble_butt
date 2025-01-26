2e0ff131cd7a6721da580e99c48d89ac7608f939
Darius <dfunches@silverstarbrands.com>
2025-01-26 00:28:04 -0600

    added upgrade system

---
+++ Assets/Assets/Scripts/UpgradeBubbles.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Assets.Scripts
+{
+    public class UpgradeBubbles : MonoBehaviour
+    {
+        private GameObject player;
+        public float speed;
+
+        public bool seekPlayer;
+
+        private void Awake()
+        {
+            player = GameObject.Find("Player");
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+
+            if (collision.gameObject.TryGetComponent<Player>(out Player player))
+            {
+                player.TakeEnergy(1);
+                Die();
+            }
+
+        }
+
+        void FixedUpdate()
+        {
+            if (seekPlayer)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed);
+            }
+        }
+
+        // Update is called once per frame
+        void Die()
+        {
+            Destroy(gameObject);
+        }
+
+        public void Activate()
+        {
+            seekPlayer = true;
+        }
+    }
+}
